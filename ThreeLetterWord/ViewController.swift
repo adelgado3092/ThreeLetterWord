fd2bf4a71cee19fe73feb5d842f88d2717566a2c
Allen Delgado <adelgado3092@stu.d214.org>
2020-01-07 11:07:42 -0600

    Initial Commit

---
+++ ThreeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeLetterWord
+//
+//  Created by Allen Delgado on 1/7/20.
+//  Copyright © 2020 Allen Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
